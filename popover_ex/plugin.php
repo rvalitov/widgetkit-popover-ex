c1733cf2f2e7f75311cbaca87a200ca2282af9e4
Ramil Valitov <ramilvalitov@gmail.com>
2018-04-25 00:36:37 +0300

    version update to 1.2.4

--- popover_ex/plugin.php
+++ popover_ex/plugin.php
@@ -16,9 +16,9 @@ return array(
 
     'main' => 'YOOtheme\\Widgetkit\\Widget\\Widget',
 	
-	'plugin_version' => 'v1.2.3',
+	'plugin_version' => 'v1.2.4',
 	
-	'plugin_date' => '14/03/2017',
+	'plugin_date' => '25/04/2018',
 	
 	'plugin_logo' => 'https://raw.githubusercontent.com/wiki/rvalitov/widgetkit-popover-ex/images/logo.png',
 	
